363e4cf6a44f8f4b923313e173b4a3e8cdf8dc24
nganlam0403 <nganlamforwork@gmail.com>
2022-01-11 11:38:35 +0700

    fix define sql script

--- outputSQLTables.cpp
+++ outputSQLTables.cpp
@@ -21,6 +21,12 @@
 #define _STRING "STRING"
 #define _AddResponseValue 1
 #define _AddQuestionValue 1
+#define _AddColumn "ADD COLUMN "
+#define _DropTable "DROP TABLE IF EXISTS "
+#define _CreateTable "CREATE TABLE "
+#define _InsertInto "INSERT INTO "
+#define _SelectAllFrom "SELECT * FROM "
+#define _AlterTable "ALTER TABLE "
 
 using namespace rapidjson;
 using namespace std;
@@ -39,9 +45,9 @@ void NarrowDesign::OutputAllQuestionsTable(string output)
     ofstream out;
     out.open(output);
 
-    out << "DROP TABLE IF EXISTS all_questions;\n";
+    out << _DropTable << "all_questions;\n";
 
-    out << "CREATE TABLE all_questions(";
+    out << _CreateTable << "all_questions(";
     out << "\n\tQuestionID INT,";
     out << "\n\tQuestionText VARCHAR(255),";
     out << "\n\tGroupName VARCHAR(255),";
@@ -49,12 +55,12 @@ void NarrowDesign::OutputAllQuestionsTable(string output)
     out << "\n);\n";
 
     for (auto x : AllQuestions) {
-        out << "INSERT INTO all_questions VALUES (";
+        out << _InsertInto << "all_questions VALUES (";
         out << x.first << ",'" << modifyString(x.second.text) << "','";
         out << modifyString(x.second.groupName) << "'," << (x.second.isClosed ? "TRUE" : "FALSE") << ");\n";
     }
 
-    out << "SELECT * FROM all_questions;";
+    out << _SelectAllFrom << "all_questions;";
 
     out.close();
 }
@@ -62,19 +68,19 @@ void NarrowDesign::OutputAllQuestionsTable2(string output)
 {
     ofstream out;
     out.open(output);
-    out << "DROP TABLE IF EXISTS all_questions2;\n";
-    out << "CREATE TABLE all_questions2(\n";
+    out << _DropTable << "all_questions2;\n";
+    out << _CreateTable << "all_questions2(\n";
     out << "\tQuestionID INT\n";
     out << ");\n\n";
 
-    out << "ALTER TABLE all_questions2\n";
+    out << _AlterTable << "all_questions2\n";
     for (int i = 1; i < MaxLevel_; i++)
-        out << "ADD COLUMN Level" << i << " VARCHAR(255),\n";
-    out << "ADD COLUMN Level" << MaxLevel_ << " VARCHAR(255);\n\n";
+        out << _AddColumn << "Level" << i << " VARCHAR(255),\n";
+    out << _AddColumn << "Level" << MaxLevel_ << " VARCHAR(255);\n\n";
 
     for (auto x : AllQuestions2) {
         auto y = x.second.level;
-        out << "INSERT INTO all_questions2 (";
+        out << _InsertInto << "all_questions2 (";
         for (int i = 0; i < (int)y.size(); i++)
             out << "Level" << i + 1 << ',';
         out << "QuestionID)\n";
@@ -85,7 +91,7 @@ void NarrowDesign::OutputAllQuestionsTable2(string output)
         out << x.first << ");\n\n";
     }
 
-    out << "SELECT * FROM all_questions2;";
+    out << _SelectAllFrom << "all_questions2;";
 
     out.close();
 }
@@ -94,18 +100,18 @@ void NarrowDesign::OutputAllOptionsTable(string output)
     ofstream out;
     out.open(output);
 
-    out << "DROP TABLE IF EXISTS all_options;\n";
+    out << _DropTable << "all_options;\n";
 
-    out << "CREATE TABLE all_options(";
+    out << _CreateTable << "all_options(";
     out << "\n\tQuestionID INT,";
     out << "\n\tOptionID INT,";
     out << "\n\tOptionText varchar(255)";
     out << "\n);\n";
 
     for (auto x : AllOptions)
-        out << "INSERT INTO all_options VALUES (" << x.questionID << "," << x.id << ",'" << modifyString(x.text) << "');\n";
+        out << _InsertInto << "all_options VALUES (" << x.questionID << "," << x.id << ",'" << modifyString(x.text) << "');\n";
 
-    out << "SELECT * FROM all_options;";
+    out << _SelectAllFrom << "all_options;";
 
     out.close();
 }
@@ -114,20 +120,20 @@ void NarrowDesign::OutputAllClosedResponsesTable(string output)
     ofstream out;
     out.open(output);
 
-    out << "DROP TABLE IF EXISTS all_closed_responses;\n";
+    out << _DropTable << "all_closed_responses;\n";
 
-    out << "CREATE TABLE all_closed_responses(";
+    out << _CreateTable << "all_closed_responses(";
     out << "\n\tParticipantID INT,";
     out << "\n\tQuestionID INT,";
     out << "\n\tOptionID INT";
     out << "\n);\n";
 
     for (auto x : closedResponses) {
-        out << "INSERT INTO all_closed_responses VALUES (";
+        out << _InsertInto << "all_closed_responses VALUES (";
         out << x.id << "," << x.questionID << ",'" << x.optionID << "');\n";
     }
 
-    out << "SELECT * FROM all_closed_responses;";
+    out << _SelectAllFrom << "all_closed_responses;";
 
     out.close();
 }
@@ -136,9 +142,9 @@ void NarrowDesign::OutputAllOpenedResponsesTable(string output)
     ofstream out;
     out.open(output);
 
-    out << "DROP TABLE IF EXISTS all_opened_responses;\n";
+    out << _DropTable << "all_opened_responses;\n";
 
-    out << "CREATE TABLE all_opened_responses(";
+    out << _CreateTable << "all_opened_responses(";
     out << "\n\tParticipantID INT,";
     out << "\n\tQuestionID INT,";
     out << "\n\tResponseText varchar(255),";
@@ -146,11 +152,11 @@ void NarrowDesign::OutputAllOpenedResponsesTable(string output)
     out << "\n);\n";
 
     for (auto x : openedResponses) {
-        out << "INSERT INTO all_opened_responses VALUES (";
+        out << _InsertInto << "all_opened_responses VALUES (";
         out << x.id << "," << x.questionID << ",'" << modifyString(x.responseText) << "','" << x.responseType << "');\n";
     }
 
-    out << "SELECT * FROM all_opened_responses;";
+    out << _SelectAllFrom << "all_opened_responses;";
 
     out.close();
 }
